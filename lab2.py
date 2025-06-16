fd309985e1856f6db91005ce31ddd5cc2979bdd5
Maite Ramirez <maiteramirez@Maites-MacBook-Pro.local>
2025-06-16 13:30:35 -0700

    completed lab2

---
+++ lab2.py
@@ -0,0 +1,19 @@
+# Lab 2
+
+## Step 1
+name = "Maite Ramirez"
+
+## step 2
+nickname = 'May'
+
+## Step 3
+age = 15
+
+## Step 4
+used_python = False
+
+## Step 5
+hobbies = ["drawing", "crocheting", "video games", "playing my guitar"]
+
+## Step 6
+favorite_things = {"movie": "None", 'food': 'pizza', 'animal': 'crow', 'color': 'green'}
