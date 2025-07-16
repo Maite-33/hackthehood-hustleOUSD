dadcbf1d802e9191c66bf42225ec2208404820b9
Maite Ramirez <maiteramirez@Maites-MacBook-Pro.local>
2025-07-16 15:04:07 -0700

    Finished Superhero-Simulator

---
+++ superhero-simulator/ability.py
@@ -0,0 +1,15 @@
+import random
+
+class Ability:
+    def __init__(self, name, max_damage): 
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        '''Returns a random integer between 0 to max_damage'''
+        return random.randint(0, self.max_damage)
+
+
+if __name__ == "__main__":
+    fireball = Ability("Fireball", 50)
+    print(fireball.attack())
---
+++ superhero-simulator/hero.py
@@ -0,0 +1,51 @@
+# Lab 6 -- Maite R.
+
+from ability import Ability
+from armor import Armor
+import random
+
+
+class Hero: 
+    def __init__(self, name, starting_health=100):
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
+
+    def battle(self, opponent):
+        winner = random.choice([self.name, opponent.name])
+        print(f"{winner} has won the battle!")
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def sum_of_attacks(self):
+        total_damage = 0
+
+        for ability in self.abilities:
+            total_damage += ability.attack()
+
+        return total_damage
+        
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def defend(self):
+        total_block = 0 
+
+        for armor in self.armors:
+            total_block += armor.block()
+
+        return total_block
+
+
+if __name__ == "__main__":
+    my_hero = Hero("Grace Hopper", 200)
+    my_hero2 = Hero("Superman", 500)
+    print(my_hero.name)            # Grace Hopper
+    print(my_hero.current_health)  # 200
+    my_hero.battle(my_hero2)
+
+
+
